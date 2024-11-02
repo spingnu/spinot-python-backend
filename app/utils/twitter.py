8f796c69df8f969d9d51d647c26cf6ebba7a211e
Kim Jae Jun <rlawowns97@naver.com>
2024-11-02 22:38:09 +0900

    #2 : add start_time when fetching timeline info

--- app/utils/twitter.py
+++ app/utils/twitter.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
 import logging
+from datetime import datetime
 
 from pytwitter import Api
 
 
-def fetch_twitter_user_home_timelines(access_token: str, account_id: str):
+def fetch_twitter_user_home_timelines(
+    access_token: str, account_id: str, start_time: str
+):
     try:
         api = Api(bearer_token=access_token)
-        response = api.get_timelines(user_id=account_id)
+        response = api.get_timelines(user_id=account_id, start_time=start_time)
         home_timelines = response.data
 
         result = []
@@ -22,10 +25,15 @@ def fetch_twitter_user_home_timelines(access_token: str, account_id: str):
 
 
 def fetch_twitter_home_timelines(twitter_users):
+    current_time = datetime.utcnow()
+    start_time = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
     home_timelines = []
     for twitter_user in twitter_users:
         access_token = twitter_user.get("access_token")
         account_id = twitter_user.get("provider_account_id")
-        timelines = fetch_twitter_user_home_timelines(access_token, account_id)
+        timelines = fetch_twitter_user_home_timelines(
+            access_token, account_id, start_time
+        )
         home_timelines.append({"account_id": account_id, "timelines": timelines})
     return home_timelines
