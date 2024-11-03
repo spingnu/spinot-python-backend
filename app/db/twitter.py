960120c47f13266ac80b2cbb4ae44209b21c1758
Kim Jae Jun <rlawowns97@naver.com>
2024-11-03 16:36:42 +0900

    #2 : detach access to tweet table logic

--- app/db/twitter.py
+++ app/db/twitter.py
@@ -4,27 +4,6 @@ from app.logger import logger
 from app.supabase_client import supabase
 
 
-def is_exist_tweet(tweet_id: str):
-    is_exist = (
-        supabase.table("tweet")
-        .select("*", count="exact")
-        .eq("tweet_id", tweet_id)
-        .execute()
-    )
-
-    return True if is_exist.count > 0 else False
-
-
-def insert_tweet(tweet_id: str, content: str):
-    insert_data = {"tweet_id": tweet_id, "content": content}
-    try:
-        supabase.table("tweet").insert(insert_data).execute()
-        return True
-    except Exception as e:
-        logger.error(f"fail to insert_tweet (error={e})")
-    return False
-
-
 def is_exist_tweets_users(user_id: str, tweet_id: str):
     is_exist = (
         supabase.table("tweets_users")
